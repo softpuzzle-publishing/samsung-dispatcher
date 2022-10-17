c368f9d1d18f271bb200d96991c59e58ca194f71
park <rmstn133@gmail.com>
2022-10-18 00:16:24 +0900

    no message

--- dist/assets/js/ui.js
+++ dist/assets/js/ui.js
@@ -1,1 +1,32 @@
 "use strict";
+
+$(document).ready(function () {
+  //  group 오픈
+  $(".open-group").click(function () {
+    $(".container-map").hide();
+    $("#aside-group").show();
+    $("#aside-group-detail").hide();
+  }); //  group 상세 오픈
+
+  $(".open-group-detail").click(function () {
+    // 임시
+    $(".container-map").hide();
+    $("#aside-group").hide();
+    $("#aside-group-detail").show();
+  }); //   content-right 접기
+
+  $(".fold-btn").click(function () {
+    // 임시
+    $(".container-map").hide();
+    $(".content-right").hide();
+  }); //    group 상세 접기
+
+  $(".detail-back").click(function () {
+    // 임시
+    $(".container-map").hide();
+    $("#aside-group").show();
+    $("#aside-group-detail").hide();
+  });
+  $("#datepicker-as").datepicker();
+  $("#datepicker-is").datepicker(); //   container-map은 나중에 다 빼기
+});
