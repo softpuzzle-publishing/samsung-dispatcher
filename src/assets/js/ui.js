d20230e164632a4c1bae82ea064df02674ae20b6
KIM HANA <hnone99@gmail.com>
2022-10-18 19:51:59 +0900

    레이아웃 변경

--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -1,33 +1,26 @@
 $(document).ready(function () {
   //  group 오픈
   $(".open-group").click(function () {
-    $(".container-map").hide();
+    $("html").removeAttr("class");
+    $("html").addClass("open-aside-group");
     $(".btn-menu").removeClass("active");
-    $(".content-right").hide();
-    $("#aside-group").show();
   });
 
   //  group 상세 오픈
   $(".open-group-detail").click(function () {
-    // 임시
-    $(".container-map").hide();
-
-    $(".content-right").hide();
-    $("#aside-group-detail").show();
+    $("html").removeAttr("class");
+    $("html").addClass("open-aside-group-detail");
   });
+
   //  contacts 오픈
   $(".open-contacts").click(function () {
-    $(".container-map").hide();
-
-    $(".content-right").hide();
-    $("#aside-contacts").show();
+    $("html").removeAttr("class");
+    $("html").addClass("open-aside-contacts");
   });
 
   //   content-right 접기
-  $(".fold-btn").click(function () {
-    // 임시
-    $(".container-map").hide();
-    $(".content-right").hide();
+  $(".right-fold-btn").click(function () {
+    $("html").removeAttr("class");
   });
 
   //    group 상세 접기
