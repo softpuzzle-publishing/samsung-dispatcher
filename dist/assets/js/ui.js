2295ec93ceb99d4810cbafc73062ed49ac9441db
KIM HANA <hnone99@gmail.com>
2022-11-17 18:05:29 +0900

    수정

--- dist/assets/js/ui.js
+++ dist/assets/js/ui.js
@@ -42,7 +42,6 @@ asideOpenBtns.forEach(function (btn) {
     }
 
     isPanel = isPanelName; //현재 panel 저장
-    document.querySelector(".btn-menu").classList.remove("active"); //메뉴레이어 hide
     console.log("prev : " + previousPanel + " / is : " + isPanel);
   });
 });
@@ -83,66 +82,93 @@ document.addEventListener("mouseup", function (e) {
 });
 
 //multiple selection checkbox
+var cards = document.querySelectorAll(".group-list .card");
 var multiSelCheck = document.querySelector("#multipleCheck");
 multiSelCheck.addEventListener("change", function () {
-  var disabledBtns = document.querySelectorAll(".multiple-function");
+  cards.forEach(function (card) {
+    card.classList.remove("selected");
+  });
   if (this.checked) {
     document.querySelector(".group-list").classList.add("selectable");
-    disabledBtns.forEach(function (btn) {
-      btn.disabled = false;
-    });
   } else {
     document.querySelector(".group-list").classList.remove("selectable");
-    disabledBtns.forEach(function (btn) {
-      btn.disabled = true;
-    });
-    var _cards = document.querySelectorAll(".group-list .card");
-    _cards.forEach(function (card) {
-      card.classList.remove("selected");
-    });
   }
 });
 // card - selection
-var cards = document.querySelectorAll(".group-list .card");
 cards.forEach(function (card) {
   card.addEventListener("click", function () {
     var selectedCard = this;
+    var selectedCards = document.querySelectorAll(".group-list .selected");
+    //multi
     if (document.querySelector(".group-list").classList.contains("selectable")) {
       selectedCard.classList.toggle("selected");
-    } else {
-      cards.forEach(function (card) {
+      selectedCards = document.querySelectorAll(".group-list .selected");
+      // 선택된 카드 갯수
+      document.querySelector(".selected-item-length .length").innerHTML = selectedCards.length;
+      // 카드가 1개라도 선택되면 상단에 레이어 bar 노출
+      if (selectedCards.length > 0) {
+        document.querySelector(".top-bar").classList.add("show");
+      } else {
+        document.querySelector(".top-bar").classList.remove("show");
+      }
+    }
+    //single
+    else {
+      selectedCard.classList.toggle("selected");
+      selectedCards.forEach(function (card) {
         card.classList.remove("selected");
       });
-      selectedCard.classList.add("selected");
     }
   });
+});
+//top bar close button
+document.querySelector(".btn-top-bar-close").addEventListener("click", function () {
+  document.querySelector(".top-bar").classList.remove("show");
+  cards.forEach(function (card) {
+    card.classList.remove("selected");
+  });
+  multiSelCheck.checked = false;
+  document.querySelector(".group-list").classList.remove("selectable");
 });
 $("[data-picker='date']").datepicker();
 
 /* card - favorite 토글 */
 var favoriteBtns = document.querySelectorAll(".btn-favorites");
 favoriteBtns.forEach(function (btn) {
-  btn.addEventListener("click", function () {
+  btn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    console.log("즐겨찾기 해제 & 아이콘 hide");
     this.classList.toggle("active");
+    this.remove();
   });
 });
 
 /* card - 발언 토글 */
-var speakingBtns = document.querySelectorAll(".btn-call-speaking button");
-speakingBtns.forEach(function (btn) {
+/* const speakingBtns = document.querySelectorAll(".btn-call-speaking button");
+speakingBtns.forEach((btn) => {
   btn.addEventListener("click", function () {
     this.classList.toggle("active");
   });
-});
+}); */
 var pttBtn = document.querySelector(".btn-ptt");
 pttBtn.addEventListener("click", function () {
   this.classList.toggle("active");
 });
 
+/* card - 종료 */
+var callEndBtns = document.querySelectorAll(".btn-call-end button");
+callEndBtns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    console.log("종료 버튼 clicked");
+  });
+});
+
 /* card - 가로채기 토글 */
 var interceptionBtns = document.querySelectorAll(".btn-call-interception button");
 interceptionBtns.forEach(function (btn) {
-  btn.addEventListener("click", function () {
+  btn.addEventListener("click", function (e) {
+    e.stopPropagation();
     this.classList.toggle("active");
   });
 });
@@ -150,22 +176,18 @@ interceptionBtns.forEach(function (btn) {
 /* card - 메뉴 */
 var cardMenuBtns = document.querySelectorAll(".btn-card-menu");
 cardMenuBtns.forEach(function (btn) {
-  btn.addEventListener("click", function () {
-    var myCard = this.closest(".card");
-    myCard.querySelector(".card-menu-list").classList.toggle("active");
-  });
-});
-var cardMenuCLoseBtns = document.querySelectorAll(".btn-card-menu-close");
-cardMenuCLoseBtns.forEach(function (btn) {
-  btn.addEventListener("click", function () {
-    this.closest(".card-menu-list").classList.remove("active");
+  btn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    console.log("카드 메뉴 버튼 clicked");
   });
 });
 
-/* card - 사운드 toggle */
-var soundBtns = document.querySelectorAll(".sound button");
-soundBtns.forEach(function (btn) {
+/* 검색 input 활성화 */
+var searchBtns = document.querySelectorAll(".search-form");
+searchBtns.forEach(function (btn) {
   btn.addEventListener("click", function () {
-    this.classList.toggle("off");
+    console.log(1);
+    this.classList.add("show");
+    this.querySelector(".form-control").focus();
   });
 });
--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -39,7 +39,6 @@ asideOpenBtns.forEach((btn) => {
       previousPanel = isPanel; //이전 panel 저장
     }
     isPanel = isPanelName; //현재 panel 저장
-    document.querySelector(".btn-menu").classList.remove("active"); //메뉴레이어 hide
     console.log("prev : " + previousPanel + " / is : " + isPanel);
   });
 });
@@ -80,39 +79,53 @@ document.addEventListener("mouseup", function (e) {
 });
 
 //multiple selection checkbox
+const cards = document.querySelectorAll(".group-list .card");
 const multiSelCheck = document.querySelector("#multipleCheck");
 multiSelCheck.addEventListener("change", function () {
-  let disabledBtns = document.querySelectorAll(".multiple-function");
+  cards.forEach((card) => {
+    card.classList.remove("selected");
+  });
   if (this.checked) {
     document.querySelector(".group-list").classList.add("selectable");
-    disabledBtns.forEach((btn) => {
-      btn.disabled = false;
-    });
   } else {
     document.querySelector(".group-list").classList.remove("selectable");
-    disabledBtns.forEach((btn) => {
-      btn.disabled = true;
-    });
-    let cards = document.querySelectorAll(".group-list .card");
-    cards.forEach((card) => {
-      card.classList.remove("selected");
-    });
   }
 });
 // card - selection
-const cards = document.querySelectorAll(".group-list .card");
 cards.forEach((card) => {
   card.addEventListener("click", function () {
     const selectedCard = this;
+    let selectedCards = document.querySelectorAll(".group-list .selected");
+    //multi
     if (document.querySelector(".group-list").classList.contains("selectable")) {
       selectedCard.classList.toggle("selected");
-    } else {
-      cards.forEach((card) => {
+      selectedCards = document.querySelectorAll(".group-list .selected");
+      // 선택된 카드 갯수
+      document.querySelector(".selected-item-length .length").innerHTML = selectedCards.length;
+      // 카드가 1개라도 선택되면 상단에 레이어 bar 노출
+      if (selectedCards.length > 0) {
+        document.querySelector(".top-bar").classList.add("show");
+      } else {
+        document.querySelector(".top-bar").classList.remove("show");
+      }
+    }
+    //single
+    else {
+      selectedCard.classList.toggle("selected");
+      selectedCards.forEach((card) => {
         card.classList.remove("selected");
       });
-      selectedCard.classList.add("selected");
     }
   });
+});
+//top bar close button
+document.querySelector(".btn-top-bar-close").addEventListener("click", function () {
+  document.querySelector(".top-bar").classList.remove("show");
+  cards.forEach((card) => {
+    card.classList.remove("selected");
+  });
+  multiSelCheck.checked = false;
+  document.querySelector(".group-list").classList.remove("selectable");
 });
 
 $("[data-picker='date']").datepicker();
@@ -120,27 +133,40 @@ $("[data-picker='date']").datepicker();
 /* card - favorite 토글 */
 const favoriteBtns = document.querySelectorAll(".btn-favorites");
 favoriteBtns.forEach((btn) => {
-  btn.addEventListener("click", function () {
+  btn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    console.log("즐겨찾기 해제 & 아이콘 hide");
     this.classList.toggle("active");
+    this.remove();
   });
 });
 
 /* card - 발언 토글 */
-const speakingBtns = document.querySelectorAll(".btn-call-speaking button");
+/* const speakingBtns = document.querySelectorAll(".btn-call-speaking button");
 speakingBtns.forEach((btn) => {
   btn.addEventListener("click", function () {
     this.classList.toggle("active");
   });
-});
+}); */
 const pttBtn = document.querySelector(".btn-ptt");
 pttBtn.addEventListener("click", function () {
   this.classList.toggle("active");
 });
 
+/* card - 종료 */
+const callEndBtns = document.querySelectorAll(".btn-call-end button");
+callEndBtns.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    console.log("종료 버튼 clicked");
+  });
+});
+
 /* card - 가로채기 토글 */
 const interceptionBtns = document.querySelectorAll(".btn-call-interception button");
 interceptionBtns.forEach((btn) => {
-  btn.addEventListener("click", function () {
+  btn.addEventListener("click", function (e) {
+    e.stopPropagation();
     this.classList.toggle("active");
   });
 });
@@ -148,22 +174,18 @@ interceptionBtns.forEach((btn) => {
 /* card - 메뉴 */
 const cardMenuBtns = document.querySelectorAll(".btn-card-menu");
 cardMenuBtns.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    const myCard = this.closest(".card");
-    myCard.querySelector(".card-menu-list").classList.toggle("active");
-  });
-});
-const cardMenuCLoseBtns = document.querySelectorAll(".btn-card-menu-close");
-cardMenuCLoseBtns.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    this.closest(".card-menu-list").classList.remove("active");
+  btn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    console.log("카드 메뉴 버튼 clicked");
   });
 });
 
-/* card - 사운드 toggle */
-const soundBtns = document.querySelectorAll(".sound button");
-soundBtns.forEach((btn) => {
+/* 검색 input 활성화 */
+const searchBtns = document.querySelectorAll(".search-form");
+searchBtns.forEach((btn) => {
   btn.addEventListener("click", function () {
-    this.classList.toggle("off");
+    console.log(1);
+    this.classList.add("show");
+    this.querySelector(".form-control").focus();
   });
 });
